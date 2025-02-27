86ad119eb66cfff36819942d1636f0707c1183d7
Gustavo Olmo <gstvo2k15@gmail.com>
2025-02-27 14:09:03 +0100

    Fix provider auth_url var

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,6 @@
 provider "openstack" {
   # checkov:skip=CKV_OPENSTACK_1: "Ensure no hard coded OpenStack password, token, or application_credential_secret exists in provider"
-  auth_url    = var.os_url
+  auth_url    = var.auth_url
   tenant_name = var.tenant_name
   user_name   = var.user_name
   password    = nonsensitive(var.password)
