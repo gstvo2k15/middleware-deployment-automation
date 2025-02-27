3916aefb14b9f9b68655c553a44c4295cac722d7
Gustavo Olmo <gstvo2k15@gmail.com>
2025-02-27 12:31:53 +0100

    Include required vars for VM creation and gitignore for rsa.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,15 @@ variable "password" {}
 variable "region" {
   default = "RegionOne"
 }
+
+variable "purpose" {
+  description = "Purpose of the VPC (e.g., dev, prod)"
+  type        = string
+  default     = "middleware"
+}
+
+variable "network_prefix" {
+  description = "Base CIDR for the VPC subnet"
+  type        = string
+  default     = "10.0.1.0"
+}
