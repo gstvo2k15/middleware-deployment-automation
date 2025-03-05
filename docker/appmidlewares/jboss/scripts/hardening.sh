174ba5112cf3de1efcbb36ce059f0c1afa7ed85e
Gustavo Olmo <gstvo2k15@gmail.com>
2025-03-05 16:27:20 +0100

    Include newest fixes for apache & nginx-2.

--- docker/appmidlewares/jboss/scripts/hardening.sh
+++ docker/appmidlewares/jboss/scripts/hardening.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "Aplicando medidas de seguridad..."
-
 # Disable automatic deployments
 sed -i 's/scan-enabled="true"/scan-enabled="false"/g' /opt/wildfly/standalone/configuration/standalone.xml
 
@@ -12,4 +10,11 @@ sed -i 's|<subsystem xmlns="urn:jboss:domain:remoting:5.0">|<subsystem xmlns="ur
 # Config TLS
 sed -i 's|<keystore path=.*|<keystore path="/etc/pki/tls/private/wildfly-keystore.jks" keystore-password="SuperSecurePass"/>|g' /opt/wildfly/standalone/configuration/standalone.xml
 
-echo "Hardening completado."
+# Generate Keystore in runtime if not exist
+if [ ! -f /etc/pki/tls/private/wildfly-keystore.jks ]
+    then
+        echo "🔑 Generando keystore para TLS..."
+        keytool -genkey -alias wildfly -keyalg RSA -keystore /etc/pki/tls/private/wildfly-keystore.jks \
+            -storepass SuperSecurePass -keypass SuperSecurePass -validity 365 \
+            -dname "CN=localhost, OU=DevOps, O=MyCompany, L=City, ST=State, C=US"
+fi
