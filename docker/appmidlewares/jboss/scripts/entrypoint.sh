c8206e1b7674a659d5bcde6fdccd031a6b279d98
Gustavo Olmo <gstvo2k15@gmail.com>
2025-03-06 12:47:41 +0100

    Starting with basics for web-middlewares-1b

--- docker/appmidlewares/jboss/scripts/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Apply specific config before start WildFly
-/scripts/hardening.sh
-
-echo "Arrancando WildFly con seguridad aplicada..."
-exec /opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement=127.0.0.1
