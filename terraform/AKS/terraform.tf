a76f37a430fcdbc95f5da98fec5e517e03f51c89
Gustavo Olmo <gstvo2k15@gmail.com>
2025-03-07 12:38:25 +0100

    Include terraform 1.8.1 as default required version

--- terraform/AKS/terraform.tf
+++ terraform/AKS/terraform.tf
@@ -1,4 +1,22 @@
 terraform {
+  required_version = "= 1.8.1"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100.0"
+    }
+    random = {
+      version = "~> 3.1.0"
+    }
+    null = {
+      version = "~> 3.1.0"
+    }
+    ansible = {
+      source  = "ansible/ansible"
+      version = "1.3.0"
+    }
+  }
   backend "azurerm" {
     resource_group_name  = "middleware-rg"
     storage_account_name = "middlewaretfstate"
