16defc05fc66f2301997b638f1187be0b3138f12
Gustavo Olmo <gstvo2k15@gmail.com>
2025-03-07 12:49:06 +0100

    Remove duplicated backend in main.tf of project.

--- terraform/AKS/main.tf
+++ terraform/AKS/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "middleware-rg"
-    storage_account_name = "middlewaretfstate"
-    container_name       = "tfstate"
-    key                  = "aks.tfstate"
-  }
-}
-
 resource "azurerm_resource_group" "middleware_rg" {
   name     = "middleware-rg"
   location = "East US"
