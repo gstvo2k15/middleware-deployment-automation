63cc74dc3797befbc1bd50879342ce536a3df26f
Gustavo Olmo <gstvo2k15@gmail.com>
2025-03-07 14:02:52 +0100

    Include naming for cluster node pool of AKS compliance with azure reqs

--- terraform/AKS/vm.tf
+++ terraform/AKS/vm.tf
@@ -1,5 +1,5 @@
 resource "azurerm_kubernetes_cluster_node_pool" "aks_nodes" {
-  name                  = "middlewarepool"
+  name                  = "mdwpool01"
   kubernetes_cluster_id = module.aks.cluster_id
   vm_size               = "Standard_D2s_v3"
   node_count            = 3
