e9d8af7db70527d71c7ca2dfcd2d7acfdafde871
Gustavo Olmo <gstvo2k15@gmail.com>
2025-03-07 15:41:55 +0100

    Include pending VNet vars for project.

--- terraform/AKS/modules/aks/variables.tf
+++ terraform/AKS/modules/aks/variables.tf
@@ -42,3 +42,27 @@ variable "subnet_id" {
   description = "Subnet ID for AKS"
   type        = string
 }
+
+variable "vnet_name" {
+  description = "Virtual Network Name"
+  type        = string
+  default     = "middleware-vnet"
+}
+
+variable "subnet_name" {
+  description = "Subnet Name"
+  type        = string
+  default     = "middleware-subnet"
+}
+
+variable "address_space" {
+  description = "VNET Address Space"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnet_prefix" {
+  description = "Subnet Prefix"
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
